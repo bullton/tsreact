7250be396175c2fef501c5e0fef480873537e90b
Yulin Xian <yulin.xian@gmail.com>
2023-03-13 13:10:41 +0800

    update menu

--- frontend/src/layouts/mainLayout/MainLayout.tsx
+++ frontend/src/layouts/mainLayout/MainLayout.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import logo from "../../assets/images/logo.svg";
+import styles from "./MainLayout.module.css";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 // import styles from "./MainLayout.module.css";
 // import { Header, Footer } from "../../componments";
 
@@ -7,12 +10,14 @@ import {
     FileOutlined,
     HomeOutlined,
     CreditCardOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
     PieChartOutlined,
     TeamOutlined,
     UserOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme, Row, Col } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Row, Col, Typography, Button } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -50,14 +55,36 @@ interface PropsTypes {
 }
 
 export const MainLayout: React.FC<PropsTypes> = ({ children }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const params = useParams();
+
     const [collapsed, setCollapsed] = useState<boolean>(false);
+    // const [menuCollapsed, setMenuCollapsed] = useState<boolean>(false);
+    // const [, setCollapsed] = useState<boolean>(false);
+
+    const onClick: MenuProps['onClick'] = (e) => {
+        console.log('click ', e);
+        switch(e.key) {
+            case '3': navigate("/");break;
+            case '6': navigate("/mcc");break;
+            default: navigate("/");
+        }
+      };
 
     return (
         <>
             <Layout style={{ minHeight: '100vh' }}>
                 <Sider collapsible collapsed={collapsed} onCollapse={(value: boolean) => setCollapsed(value)}>
-                    <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                    <div style={{ height: 64, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }}>
+                        <span onClick={() => navigate("/")}>
+                            <img src={logo} alt="logo" className={styles["App-logo"]} />
+                            <Typography.Title level={3} className={styles.title}>
+                                牛数据
+                            </Typography.Title>
+                        </span>
+                    </div>
+                    <Menu theme="dark" defaultSelectedKeys={['6']} defaultOpenKeys={['sub2']} mode="inline" items={items} onClick={onClick}/>
                 </Sider>
                 <Layout className="site-layout">
                     <Header style={{ padding: 0, background: 'rgba(255, 255, 255, 0.2)' }} />
