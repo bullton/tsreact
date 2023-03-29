384aaed33bdaaf6c799e2a30c10c58e39294e5f1
Neil <yulin.xian@gmail.com>
2023-03-30 01:42:09 +0800

    fix line chart issue

--- frontend/src/common/charts/Line.ts
+++ frontend/src/common/charts/Line.ts
@@ -4,17 +4,13 @@ export const lineChart = (data:object[], target:string, x:string, y:string, colo
     const chart = new Chart({
         container: target,
         autoFit: true,
-        paddingLeft: 50,
+        paddingLeft: 80,
         theme: 'classic'
-        // height: window.innerHeight,
-        // padding: [20, 210, 30, 50]
       });
 
     chart
         .line() // 创建一个 Interval 标记
         .data(data) // 绑定数据
-        // .transform({ type: 'sortX', by: 'x', reverse: false })
-        // .transform({ type: 'dodgeX' })
         .encode('x', x)
         .encode('y', y)
         .encode('color', colorField)
@@ -26,11 +22,12 @@ export const lineChart = (data:object[], target:string, x:string, y:string, colo
                 dy: -5
             }
         })
-        .axis('y', { title: '↑ 二手房挂牌量 (套)' });
+        .scrollbar('x', {})
+        .axis('x', { title: '↑ 挂牌日期', grid: true, line: true })
+        .axis('y', { title: '↑ 二手房挂牌量 (套)', line: true });
         
 
     chart.interaction('tooltip', {
-        // 设置 Tooltip 的位置，为 'auto' 时会自动调整 Tooltip 使其不会超出图表区域
         position: 'auto',
       });
     chart.render();
