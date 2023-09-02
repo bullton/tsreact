3323ab8b1cabf47aa6cbd07cdcde6d0e3757b6ec
Yulin Xian <yulin.xian@gmail.com>
2023-09-02 17:29:07 +0100

    change proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "http://localhost:3001", // 后台服务地址以及端口号
+            target: "http://107.174.247.111:3001", // 后台服务地址以及端口号
             changeOrigin: true, // 是否开启代理
             pathRewrite: {
                 "^/api": "/api", // 代理名称
